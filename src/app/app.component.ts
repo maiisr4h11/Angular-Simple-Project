ba57da5de49310d12baa603dea6d0affacecaebf
maiisr4h11 <maisarahibrahim2511@gmail.com>
2024-12-04 01:32:24 +0800

    import and export dummy data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({ //decorator
   selector: 'app-root', //tell angular which element it should look in html code 
@@ -9,4 +10,6 @@ import { UserComponent } from './user/user.component';
   templateUrl: './app.component.html', // markup is stored in this file
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+}
