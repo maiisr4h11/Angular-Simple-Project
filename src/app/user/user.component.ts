b2ed7a890fe69336b5ebb7e3838e21a24968f2e6
maiisr4h11 <maisarahibrahim2511@gmail.com>
2024-12-04 00:46:10 +0800

    define array and onclick function

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import  {DUMMY_USERS} from '../dummy-users';
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length); // should give random number from 0 to 5 as we have 6 users
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,4 +10,12 @@ import { Component } from '@angular/core';
 })
 export class UserComponent {
 
+  selectedUser = DUMMY_USERS[randomIndex];
+  get imagePath(){
+    return 'assets/users/' + this.selectedUser.avatar
+    //this is to access properties of a class from within the same class
+  };
+  onSelectUser() {
+    console.log("Clicked!")
+  }
 }
